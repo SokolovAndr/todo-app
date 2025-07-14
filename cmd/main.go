58fbef3d87461c935cdad1760d786db799e8dfc1
SokolovAndr <korch45@yandex.ru>
2025-07-14 16:41:45 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/SokolovAndr/todo-app"
+	"github.com/SokolovAndr/todo-app/pkg/handler"
+)
+
+func main() {
+
+	handlers := new(handler.Handler)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server %s", err.Error())
+	}
+}
