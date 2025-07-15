547ae0e0a1437789bfe3eec42e73fe16001e832c
SokolovAndr <korch45@yandex.ru>
2025-07-15 17:51:42 +0300

    4th commit

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,27 @@ import (
 	"github.com/SokolovAndr/todo-app/pkg/handler"
 	"github.com/SokolovAndr/todo-app/pkg/repository"
 	"github.com/SokolovAndr/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+
+	if err := intiConfig(); err !=nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server %s", err.Error())
 	}
+}
+
+func intiConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
