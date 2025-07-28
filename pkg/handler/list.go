eedfc8d989317d4b3627fe5993ed2d6a868cdf84
SokolovAndr <korch45@yandex.ru>
2025-07-28 15:52:42 +0300

    10th commit

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -3,11 +3,25 @@ package handler
 import (
 	"net/http"
 
+	"github.com/SokolovAndr/todo-app"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) createList(c *gin.Context) {
-	id, _ := c.Get(userCtx)
+	userId, err := getUsrId(c)
+	if err != nil {
+		return
+	}
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err != nil {
+		newErrorRespose(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	id, err := h.services.TodoList.Create(userId, input)
+	if err != nil {
+		newErrorRespose(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"id": id,
 	})
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -12,6 +12,7 @@ type Authorization interface {
 }
 
 type TodoList interface {
+	Create(userId int, list todo.TodoList) (int, error)
 }
 
 type TodoItem interface {
@@ -24,5 +25,7 @@ type Service struct {
 }
 
 func NewService(repos *repository.Repository) *Service {
-	return &Service{Authorization: NewAuthService(repos.Authorization)}
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+		TodoList:      NewTodoListService(repos.TodoList)}
 }
